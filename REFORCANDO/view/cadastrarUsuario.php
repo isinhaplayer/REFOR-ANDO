752a3bb62bc1cd6ed9e0353692b492cd963106af
isinhaplayer <166158495+isinhaplayer@users.noreply.github.com>
2024-05-06 21:54:44 -0300

    Add files via upload

---
+++ REFORCANDO/view/cadastrarUsuario.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="">
+    <link rel="stylesheet" href="../css/cadastrarUsuario.css">
+    <title>Document</title>
+</head>
+<body>
+    
+    <div class="container"><!--INICIO CONTAINER-->
+
+    <div class="container_cadastro"><!--INICIO CONTAINER CADASTRO-->
+
+    <div class="ladoEsquerdo"><!--INICIO LADO ESQUERDO-->
+        <img src="../imagem/info.jpg" alt="">
+    </div><!--FINAL LADO ESQUERDO-->
+
+    <div class="ladoDireito"><!--INICIO LADO DIREITO-->
+
+    <div class="form_cadastro">
+        <h1>Cadastrar-se</h1>
+        <form action="">
+                        <p>Nome:</p>
+                        <input type="text">
+                        <p>Sobrenome:</p>
+                        <input type="text">
+                        <p>E-mail:</p>
+                        <input type="email">
+                        <p>Telefone:</p>
+                        <input type="number">
+                        <p>Data de Nascimento</p>
+                        <input type="date">
+                        <p>Senha:</p>
+                        <input type="password">
+                        <p>Confirme sua senha:</p>
+                        <input type="password">
+                        <input type="submit" value="Cadastre-se">
+                    </form>
+    </div>
+
+    </div><!--FIM LADO DIREITO-->
+
+    </div><!--FIM CONTAINER CADASTRO-->
+
+
+    </div><!-- FIM CONTAINER-->
+
+</body>
+</html>
+
+
